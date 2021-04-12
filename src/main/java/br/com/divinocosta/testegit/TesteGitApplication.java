619c7830741177cf3de7290b6566caac59313ec0
Alex Costa <alex.costa.modusbox.com>
2021-04-12 10:22:50 -0300

    criando o projeto

---
+++ src/main/java/br/com/divinocosta/testegit/TesteGitApplication.java
@@ -0,0 +1,13 @@
+package br.com.divinocosta.testegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteGitApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(TesteGitApplication.class, args);
+  }
+
+}
---
+++ src/test/java/br/com/divinocosta/testegit/TesteGitApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.divinocosta.testegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteGitApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
